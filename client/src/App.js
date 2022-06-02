ddd026ea49c04813b9cc20469637c881ebf2958b
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-06-02 21:22:15 +0900

    connectwalletボタンを実装

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
-// Constants
-const TWITTER_HANDLE = 'あなたのTwitterハンドル';
+// Constants 
+const TWITTER_HANDLE = 'HARUKI05758694';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+/**
+ * Appコンポーネント
+ */
 const App = () => {
+  // ステート変数
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  // check user have metamask
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+    try {
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+        // MetaMaskのアカウント情報を取得する。
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+          
+          if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log("Found an authorized account:", account);
+            // ステート変数を格納する。
+            setCurrentAccount(account);
+          } else {
+            console.log("No authorized account found");
+          }
+      }
+    } catch(error) {
+      console.error(error);
+    }
+  };
+
+  /**
+   * Connect Walletボタン
+   */
+   const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+      // ウォレットアドレスに対してアクセスをリクエストしています。
+      const accounts = await ethereum.request({method: "eth_requestAccounts",});
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+
   return (
     <div className="App">
       <div className="container">
@@ -14,10 +70,13 @@ const App = () => {
         <p className="header gradient-text">⚡️ METAVERSE GAME ⚡️</p>
           <p className="sub-text">プレイヤーと協力してボスを倒そう✨</p>
           <div className="connect-wallet-container">
-            <img
-              src="https://i.imgur.com/TXBQ4cC.png"
-              alt="LUFFY"
-            />
+            <img src="https://i.imgur.com/yMocj5x.png" alt="Pikachu" />
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started
+            </button>
           </div>
         </div>
         <div className="footer-container">
